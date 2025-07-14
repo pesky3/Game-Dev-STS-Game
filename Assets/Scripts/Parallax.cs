9f476325ebe14638958f652e0169215c8e188228
pesky3 <tanchienhyi5@gmail.com>
2025-07-14 15:03:08 +0800

    .

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -29,9 +29,6 @@ public class Parallax : MonoBehaviour
         for (int i = 0; layers.Length > i; i++) 
         {
             layers[i].anchoredPosition = Vector2.SmoothDamp(layers[i].anchoredPosition, initialPosition[i] + offset * multiplier[i], ref velocity[i], dampSmoothTime);
-            Debug.Log(i);
-            Debug.Log(layers[i].anchoredPosition);
-            
         }
         // I am using anchoredPosition to move the images by the anchors instead of moving the anchors themselves, if i don't do this the images may get distorted
         // this is my first time using ref and I have learnt it's used for Unity to "remember" the value of velocity even after going through functions
