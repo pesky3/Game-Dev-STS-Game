9ac57346c3de5031e46ccc0fd894327109b312e7
pesky3 <tanchienhyi5@gmail.com>
2025-07-14 12:00:31 +0800

    parallax menu first implementation

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -2,19 +2,27 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public float multiplier = 1f;
+    public RectTransform[] layers;
+    public float[] multiplier;
     public float dampSmoothTime = 1f;
 
     private Vector2 startPoint;
     private Vector2 offset;
+    private Vector2 velocity;
     void Start()
     {
         startPoint = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        for (int i = 0; layers.Length > i; i++) 
+        {
+            layers[i].anchoredPosition = Vector2.SmoothDamp(layers[i].anchoredPosition, offset * multiplier[i], ref velocity, dampSmoothTime);
+        }
+        // I am using anchoredPosition to move the images by the anchors instead of moving the anchors themselves, if i don't do this the images may get distorted
+        // this is my first time using ref and I have learnt it's used for Unity to "remember" the value of velocity even after going through functions
     }
 }
