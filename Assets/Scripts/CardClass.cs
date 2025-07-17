95373c1ca5dee8cbe0063bb15f99374165a4b6af
pesky3 <tanchienhyi5@gmail.com>
2025-07-17 14:32:17 +0800

    Card Logic pt1

--- Assets/Scripts/CardClass.cs
+++ Assets/Scripts/CardClass.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CardClass", menuName = "Scriptable Objects/CardClass")]
-public class CardClass : ScriptableObject
+public abstract class Card : ScriptableObject
 {
-    
+
+    public string cardName = "New Card";
+    public Sprite artwork;
+    public string description;
+    public int cost = 1;
+
+    public CardType cardType;
+
+
+
+    public abstract void Play(GameObject target);
+
+}
+
+public enum CardType
+{
+    Attack,
+    Skill,
+    Power,
+    Special
 }
---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour
+{
+    public string name;
+    public int maxHealth;
+    public int currentHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
